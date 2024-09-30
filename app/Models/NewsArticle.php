1360ff401762b519fb31f4e38ae57a8f6d5d5ee1
ivvi333 <86127508+ivvi333@users.noreply.github.com>
2024-09-30 21:59:20 +0300

    Added app directory

---
+++ app/Models/NewsArticle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NewsArticle extends Model
+{
+    use HasFactory;
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+}
